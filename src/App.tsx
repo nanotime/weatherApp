48d6f71422f79bf540e21e28e7105d435262cec2
Javier Marin <nano.world.contact@gmail.com>
2024-03-17 13:39:34 -0600

    WA-F09 | added geolocalization
    Signed-off-by: Javier Marin <nano.world.contact@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from 'react';
 import flatenizeTw from '@utils/flatenizeTw';
 import { Content, Sidebar } from '@organisms/index';
+import { setGeo, setLocale } from '@/Store/AppConfigs';
+import { useGeolocation } from 'react-use';
 
 import './index.css';
 
 const mainClasses = flatenizeTw([['md:grid', 'md:grid-cols-6']]);
 
+const userLocales =
+  navigator.languages && navigator.languages.length
+    ? navigator.languages
+    : navigator.language;
+
 function App() {
+  const { latitude, longitude } = useGeolocation();
+
+  useEffect(() => {
+    if (latitude && longitude) setGeo(latitude, longitude);
+    setLocale(userLocales);
+  }, [latitude, longitude]);
+
   return (
     <main className={mainClasses}>
       <Sidebar />
